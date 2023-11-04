685e68e4aadbb9e63cc7f954f9a6c232be9612af
Your Name <you@example.com>
2023-11-04 14:13:18 +0600

    odd even number using js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let num = Number(prompt());
+
+function js(a, b) {
+    if (num % 2 == 0) {
+        document .write("This number is even")
+        
+    }
+    else{
+        document .write("This number is odd")
+    }
+    
+}
+js();
